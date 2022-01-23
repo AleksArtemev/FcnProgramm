468de29fb48d5d3f13e245324f15006c0fbe0f27
Игорь <@DESKTOP-3FOCRJ0>
2022-01-23 15:06:03 +0300

    Добавьте файлы проекта.

---
+++ FcnProgramm/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FcnProgramm
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Test1(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Content = new Page_Test();
+        }
+        private void Exit_Button(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void Page_List1(object sender, RoutedEventArgs e)
+        {
+            MyFrameList.Content = new Page_List();
+        }
+
+        private void Page_Profile1(object sender, RoutedEventArgs e)
+        {
+            MyFrameList.Content = new Page_Profile();
+        }
+    }
+}
